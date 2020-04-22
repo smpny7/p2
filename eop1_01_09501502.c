5d551ec256531d40c17c6378955b4ac9ee3be62a
smpny7 <symphonymode7@icloud.com>
2020-04-22 20:05:23 +0900

    4-22

--- eop1_01_09501502.c
+++ eop1_01_09501502.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
-int main() {
-  char s[4] = {'A', 'B', 'C', '\0'};  // ← (1) & (2)
+int subst(char *str, char c1, char c2) {
+  int diff = 0;
   char *p;
-  int i, x;
 
-  p = s;
+  p = str;
+
   while (*p != '\0') {
+    if (*p == c1) {
+      *p = c2;
+      diff++;
+    }
     p++;
   }
-  x = p - s;
 
-  p = s;
-  for (i = 0; i < x + 1; i++) {  // ← (4)
-    if ('A' <= s[i] && s[i] <= 'Z') {
-      s[i] += ('a' - 'A');
-    }
-  }
-  return 0;
+  return diff;
+}
+
+int main(void) {
+  char c1 = 'A';
+  char c2 = 'B';
+  char s[80] = "ABCDE";
+
+  printf("文字列: %s\n置換文字: %c -> %c\n\n", s, c1, c2);
+  printf("実行結果: %s\n差分: %d\n", s, subst(s, c1, c2));
 }
