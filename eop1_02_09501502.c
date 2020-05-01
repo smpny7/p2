98beb22622bc0b67ce384e44a6f163d05813c76a
smpny7 <symphonymode7@icloud.com>
2020-05-01 14:31:34 +0900

    2-4 標準入力 完成

--- eop1_02_09501502.c
+++ eop1_02_09501502.c
@@ -2,20 +2,20 @@
 #include <string.h>
 
 int subst(char *str, char c1, char c2) {
-  int diff = 0;
-  char *p;
+    int diff = 0;
+    char *p;
 
-  p = str;
+    p = str;
 
-  while (*p != '\0') {
-    if (*p == c1) {
-      *p = c2;
-      diff++;
+    while (*p != '\0') {
+        if (*p == c1) {
+        *p = c2;
+        diff++;
+        }
+        p++;
     }
-    p++;
-  }
 
-  return diff;
+    return diff;
 }
 
 int split(char *str, char *ret[], char sep, int max) {
@@ -28,16 +28,42 @@ int split(char *str, char *ret[], char sep, int max) {
     return count;
 }
 
+int get_line(char *line) {
+    // if (*line == 0) {
+    //     FILE *fp;
+    //     fp = fopen("./sample.csv", "r");
+    //     if(fp == NULL) {
+    //         printf("Could not open.\n");
+    //         return -1;
+    //     }
+    // }
+    // fgets(line, 1024, stdin);
+    if (fgets(line, 1024, stdin) == NULL) {
+        return 0;
+    } else {
+        return 1;
+    }
+    // while (fgets(line, 1024, fp) != NULL) {
+    //     puts(line);
+    // }
+}
+
 int main(void) {
-    int i, max = 5;
-    char s[] = "5100046,The Bridge,1845-11-2,14 Seafield Road Longman Inverness,SEN Unit 2.0 Open";
-    subst(s, ',', '\0');
-    char *ret[5] = {0};
-    char sep = ',';
+    // int i, max = 5;
+    // char s[] = "5100046,The Bridge,1845-11-2,14 Seafield Road Longman Inverness,SEN Unit 2.0 Open";
+    // subst(s, ',', '\0');
+    // char *ret[5] = {0};
+    // char sep = ',';
 
-    printf("element: %d\n", split(s, ret, sep, max));
+    // printf("element: %d\n", split(s, ret, sep, max));
 
-    for (i = 0; i < max; i++) {
-        printf("ret[%d] = '%s'\n", i, ret[i]);
+    // for (i = 0; i < max; i++) {
+    //     printf("ret[%d] = '%s'\n", i, ret[i]);
+    // }
+
+    char line[1024] = {0};
+
+    while (get_line(line)) {
+        puts(line);
     }
 }
